34612a0d254ff96a1054e49ed4f6468e8a8d4ff3
chris teodorski <chris.teodorski@gmail.com>
2018-06-20 21:43:31 -0400

    more troubleshooting

--- fcclookup.py
+++ fcclookup.py
@@ -1,15 +1,14 @@
 from errbot import BotPlugin, botcmd
 
 
-class Example(BotPlugin):
+class fcc(BotPlugin):
     """
-    This is a very basic plugin to try out your new installation and get you started.
-    Feel free to tweak me to experiment with Errbot.
-    You can find me in your init directory in the subdirectory plugins.
+    This is a very basic plugin to search the FCC license database
+    !fcc SearchTerm 
     """
 
     @botcmd  # flags a command
-    def tryme(self, msg, args):  # a command callable with !tryme
+    def search(self, msg, args):  # a command callable with !tryme
         """
         Execute to check if Errbot responds to command.
         Feel free to tweak me to experiment with Errbot.
