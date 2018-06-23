615c448f0e769928848bacdffb0b9ba86d7f3622
Chris Teodorski <chris.teodorski@gmail.com>
2018-06-23 11:50:13 -0400

    dammit

--- fcclookup.py
+++ fcclookup.py
@@ -21,5 +21,5 @@ class fcc(BotPlugin):
         results = requests.get(url)
         json_data = json.loads(results.text)
         for i in json_data:
-            print(i)
+            print(i['Licenses']['license'])
         return 'It *works* !'
