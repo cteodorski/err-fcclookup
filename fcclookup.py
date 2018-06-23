0225fb4db56e24bdfa20f8eaed698b373ffa995d
Chris Teodorski <chris.teodorski@gmail.com>
2018-06-22 21:24:07 -0400

    more testing

--- fcclookup.py
+++ fcclookup.py
@@ -2,7 +2,8 @@ from errbot import BotPlugin, botcmd
 
 
 class fcc(BotPlugin):
-    
+
     @botcmd  # flags a command
     def fccsearch(self, msg, args):
-        return 'It *works* !'  # This string format is markdown.
+        print("Holy Shit")
+        return 'It *works* !'
